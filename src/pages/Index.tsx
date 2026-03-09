a6448ce29556737aa9573b08c259f0cc12ba6a1f
Mauri Schulz <maurischulz@gmail.com>
2026-03-09 15:05:01 -0300

    Teste isolado do Index About

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
 
 const Index = () => {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </>
   );
 };
