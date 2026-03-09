c57f341eab4cab347e208b30b23c3b36046231c4
Mauri Schulz <maurischulz@gmail.com>
2026-03-09 15:07:41 -0300

    Teste isolado do Index Geral

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,28 @@
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
+import TechStackSection from "@/components/TechStackSection";
+import ProjectsSection from "@/components/ProjectsSection";
+import ArchitectureSection from "@/components/ArchitectureSection";
+import CareerTimeline from "@/components/CareerTimeline";
+import DashboardPreview from "@/components/DashboardPreview";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <>
+    <div className="min-h-screen bg-background">
       <Navbar />
       <HeroSection />
       <AboutSection />
-    </>
+      <TechStackSection />
+      <ProjectsSection />
+      <ArchitectureSection />
+      <CareerTimeline />
+      <DashboardPreview />
+      <ContactSection />
+      <Footer />
+    </div>
   );
 };
 
